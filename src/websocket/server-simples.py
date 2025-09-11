0dfc542dd48a57ba1c4a2364b08a4a79b05fa1fc
Michel Albonico <bsd.albonico@gmail.com>
2025-09-11 10:03:27 -0300

    Update server-simples.py

--- src/websocket/server-simples.py
+++ src/websocket/server-simples.py
@@ -1,23 +1,23 @@
-import asyncio # permite programação assíncrona wait/async
-import websockets # biblioteca para trabalhar com WebSockets
+import asyncio
+import websockets
 
-clientes = set() # conjunto para armazenar clientes conectados
+clientes = set()
 
 async def echo(websocket):
     clientes.add(websocket)
     async for message in websocket:
         print(f"Mensagem {message} recebida do cliente {websocket.remote_address}")
+        await websocket.send(f"Recebi sua mensagem!")
         if clientes:
             print(f"Clientes conectados: {len(clientes)}")
             for client in clientes:
                 if client != websocket:
                     await client.send(f"Cliente {websocket.remote_address} disse: {message}")
-        await websocket.send(f"Recebi sua mensagem!")
 
 async def main():
     async with websockets.serve(echo, "localhost", 8765):
         print("Servidor WebSocket rodando em ws://localhost:8765")
-        await asyncio.Future()  # Mantém o servidor ativo
+        await asyncio.Future() 
 
 if __name__ == "__main__":
     asyncio.run(main())
